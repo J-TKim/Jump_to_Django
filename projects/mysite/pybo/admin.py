d1b52eff39d1aa9f4a0d7800a9d8383943e4ffe8
J-TKim <68172322+J-TKim@users.noreply.github.com>
2020-10-31 02:27:23 +0900

    2-3 Upload

--- projects/mysite/pybo/admin.py
+++ projects/mysite/pybo/admin.py
@@ -1,3 +1,10 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
